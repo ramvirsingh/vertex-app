e5a946915286443b84fd2acf755b7cefdf8c9642
Ram Vir Singh <rams@interrait.com>
2017-11-28 15:52:45 +0530

    router added

--- src/main/java/io/vertx/ram/MyFirstVerticle.java
+++ src/main/java/io/vertx/ram/MyFirstVerticle.java
@@ -3,18 +3,36 @@ package io.vertx.ram;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
+
 
 public class MyFirstVerticle extends AbstractVerticle {
 
   @Override
   public void start(Future<Void> fut) {
-    vertx
-        .createHttpServer()
-        .requestHandler(r -> {
-          r.response().end("<h1>Hello from my first " +
-              "Vert.x 3 application</h1>");
-        })
-        .listen(8080, result -> {
+	  
+	  // Create a router object.
+	  Router router = Router.router(vertx);
+	  
+	// Bind "/" to our hello message - so we are still compatible.
+	  router.route("/vertx").handler(routingContext -> {
+	    HttpServerResponse response = routingContext.response();
+	    response
+	        .putHeader("content-type", "text/html")
+	        .end("<h1>Hello from my first Vert.x 3 Router application</h1>");
+	  });
+	  
+
+	  // Create the HTTP server and pass the "accept" method to the request handler.
+	  vertx
+	      .createHttpServer()
+	      .requestHandler(router::accept)
+        
+        .listen(    
+        	// Retrieve the port from the configuration,
+            // default to 8080.
+                config().getInteger("http.port", 8080), result -> {
           if (result.succeeded()) {
             fut.complete();
           } else {
